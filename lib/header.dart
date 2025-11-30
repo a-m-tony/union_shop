7eb047b25308e3e7702db4eec809934b6274907a
tonscripts <tonscripts@gmail.com>
2025-11-30 22:08:06 +0000

    recreated header

---
+++ lib/header.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/header_img.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
