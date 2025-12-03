b66d6f250b8f746e45f3c23666b0a77a02cd7595
tonscripts <tonscripts@gmail.com>
2025-12-03 19:56:43 +0000

    import header

--- lib/shop_dropdown/clothing_page.dart
+++ lib/shop_dropdown/clothing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/header.dart';
 
 class ClothingPage extends StatelessWidget {
   const ClothingPage({super.key});
@@ -6,112 +7,98 @@ class ClothingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Clothing'),
-      ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Header(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/clothing.jpeg',
-                      fit: BoxFit.cover,
-                    ),
+                  child: Column(
+                    children: [
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/clothing.jpeg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/clothing3.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/hat.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/clothing3.png',
-                      fit: BoxFit.cover,
-                    ),
+                  child: Column(
+                    children: [
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/clothing2.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/clothing.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/hat2.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/hat.png',
-                      fit: BoxFit.cover,
-                    ),
+                  child: Column(
+                    children: [
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/clothing4.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/clothing4.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/hat1.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/clothing2.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/clothing.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/hat2.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/clothing4.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/clothing4.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.asset(
-                      'assets/images/hat1.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
