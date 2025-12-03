30ab1f0bbb612de4b58ba0cd282c911cd846a3d9
tonscripts <tonscripts@gmail.com>
2025-12-03 19:55:19 +0000

    import header

--- lib/About.dart
+++ lib/About.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/header.dart';
 
 class AboutPrintSnackPage extends StatelessWidget {
   const AboutPrintSnackPage({super.key});
@@ -9,8 +10,13 @@ class AboutPrintSnackPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About The Print Snack'),
       ),
-      body: const Center(
-        child: Text('This is the About The Print Snack page.'),
+      body: const Column(
+        children: [
+          Header(),
+          Center(
+            child: Text('This is the About The Print Snack page.'),
+          ),
+        ],
       ),
     );
   }
