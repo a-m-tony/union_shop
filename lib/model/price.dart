4340418be504ab506c9d2cfa43e4a7bbd197ad64
tonscripts <tonscripts@gmail.com>
2025-12-04 20:49:06 +0000

    change price

---
+++ lib/model/price.dart
@@ -0,0 +1,49 @@
+import 'package:union_shop/model/product.dart';
+
+const List<Product> products = [
+  Product(
+    title: 'T-Shirt',
+    price: '£10.00',
+    imageUrl: 'assets/images/clothing.png',
+  ),
+  Product(
+    title: 'T-Shirt',
+    price: '£10.00',
+    imageUrl: 'assets/images/clothing2.png',
+  ),
+  Product(
+    title: 'T-Shirt',
+    price: '£10.00',
+    imageUrl: 'assets/images/clothing3.png',
+  ),
+  Product(
+    title: 'Sweater',
+    price: '£25.00',
+    imageUrl: 'assets/images/sweater4.PNG',
+  ),
+  Product(
+    title: 'Sweater',
+    price: '£25.00',
+    imageUrl: 'assets/images/sweater3.png',
+  ),
+  Product(
+    title: 'Sweater',
+    price: '£25.00',
+    imageUrl: 'assets/images/sweater2.png',
+  ),
+  Product(
+    title: 'Hoodie',
+    price: '£35.00',
+    imageUrl: 'assets/images/hoodie1.png',
+  ),
+  Product(
+    title: 'Hoodie',
+    price: '£35.00',
+    imageUrl: 'assets/images/hoodie4.png',
+  ),
+  Product(
+    title: 'Hoodie',
+    price: '£35.00',
+    imageUrl: 'assets/images/hoodie3.png',
+  ),
+];
