7f11ae65b843d4cf7477adc7a649b9bd0c3d9911
tonscripts <tonscripts@gmail.com>
2025-12-04 07:58:15 +0000

    change collection

--- lib/shop_dropdown/collection_page.dart
+++ lib/shop_dropdown/collection_page.dart
@@ -20,62 +20,7 @@ class CollectionPage extends StatelessWidget {
                 crossAxisCount: 3,
                 crossAxisSpacing: 24,
                 mainAxisSpacing: 48,
-                children: const [
-                  ProductCard(
-                    title: 'Placeholder Product 1',
-                    price: '£10.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 2',
-                    price: '£15.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 3',
-                    price: '£20.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 4',
-                    price: '£25.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 5',
-                    price: '£10.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 6',
-                    price: '£15.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 7',
-                    price: '£20.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 8',
-                    price: '£25.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                  ProductCard(
-                    title: 'Placeholder Product 9',
-                    price: '£10.00',
-                    imageUrl:
-                        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
-                  ),
-                ],
+                children: const [],
               ),
             ),
           ],
