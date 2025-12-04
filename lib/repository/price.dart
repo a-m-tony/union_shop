a3f176d98e264d8898b2514fbd4b3fec2f7228a4
tonscripts <tonscripts@gmail.com>
2025-12-04 10:03:37 +0000

    change clothes

---
+++ lib/repository/price.dart
@@ -0,0 +1,38 @@
+
+import 'package:flutter/material.dart';
+import '../model/product.dart';
+
+class Cart extends ChangeNotifier {
+  final List<Product> _items = [
+    const Product(
+      title: 'T-Shirt',
+      price: '£10.00',
+      imageUrl:
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    ),
+    const Product(
+      title: 'Sweater',
+      price: '£25.00',
+      imageUrl:
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    ),
+    const Product(
+      title: 'Hoodie',
+      price: '£35.00',
+      imageUrl:
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    ),
+  ];
+
+  List<Product> get items => _items;
+
+  void add(Product product) {
+    _items.add(product);
+    notifyListeners();
+  }
+
+  void remove(Product product) {
+    _items.remove(product);
+    notifyListeners();
+  }
+}
--- lib/shop_dropdown/list_page.dart
+++ lib/shop_dropdown/list_page.dart
@@ -1,18 +1,92 @@
 import 'package:flutter/material.dart';
 import '../header.dart';
+import '../product_card.dart';
+import '../model/product.dart';
+import '../repository/price.dart';
 
 class ListPage extends StatelessWidget {
   const ListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final cart = Cart();
+    final products = [
+      Product(
+        title: 't shirts',
+        price: cart.items.firstWhere((p) => p.title == 'T-Shirt').price,
+        imageUrl: 'assets/images/sweater1.png',
+      ),
+      Product(
+        title: 'Sweater 4',
+        price: cart.items.firstWhere((p) => p.title == 'Sweater').price,
+        imageUrl: 'assets/images/sweater4.PNG',
+      ),
+      Product(
+        title: 'Sweater 3',
+        price: cart.items.firstWhere((p) => p.title == 'Sweater').price,
+        imageUrl: 'assets/images/sweater3.png',
+      ),
+      Product(
+        title: 'Sweater 2',
+        price: cart.items.firstWhere((p) => p.title == 'Sweater').price,
+        imageUrl: 'assets/images/sweater2.png',
+      ),
+      Product(
+        title: 'hoodie',
+        price: cart.items.firstWhere((p) => p.title == 'Hoodie').price,
+        imageUrl: 'assets/images/hoodie1.png',
+      ),
+      Product(
+        title: 't shirts',
+        price: cart.items.firstWhere((p) => p.title == 'T-Shirt').price,
+        imageUrl: 'assets/images/clothing3.png',
+      ),
+      Product(
+        title: 't shirts',
+        price: cart.items.firstWhere((p) => p.title == 'T-Shirt').price,
+        imageUrl: 'assets/images/clothing.png',
+      ),
+      Product(
+        title: 't shirts',
+        price: cart.items.firstWhere((p) => p.title == 'T-Shirt').price,
+        imageUrl: 'assets/images/clothing2.png',
+      ),
+      Product(
+        title: 't shirts',
+        price: cart.items.firstWhere((p) => p.title == 'T-Shirt').price,
+        imageUrl: 'assets/images/clothing3.png',
+      ),
+      Product(
+        title: 'hoodie',
+        price: cart.items.firstWhere((p) => p.title == 'Hoodie').price,
+        imageUrl: 'assets/images/hoodie1.png',
+      ),
+      Product(
+        title: 'hoodie',
+        price: cart.items.firstWhere((p) => p.title == 'Hoodie').price,
+        imageUrl: 'assets/images/hoodie2.png',
+      ),
+      Product(
+        title: 'hoodie',
+        price: cart.items.firstWhere((p) => p.title == 'Hoodie').price,
+        imageUrl: 'assets/images/hoodie3.png',
+      ),
+    ];
+
+    return Scaffold(
       body: Column(
         children: [
-          Header(),
+          const Header(),
           Expanded(
-            child: Center(
-              child: Text('List Page'),
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: products
+                  .map((product) => ProductCard(
+                        title: product.title,
+                        price: product.price,
+                        imageUrl: product.imageUrl,
+                      ))
+                  .toList(),
             ),
           ),
         ],
