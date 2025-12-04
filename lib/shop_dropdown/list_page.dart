6f2462fb1bad847ad3602e0af817a17d5ae72b65
tonscripts <tonscripts@gmail.com>
2025-12-04 09:35:40 +0000

    chnage list

--- lib/shop_dropdown/list_page.dart
+++ lib/shop_dropdown/list_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import '../header.dart';
 
 class ListPage extends StatelessWidget {
   const ListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('List'),
-      ),
-      body: const Center(
-        child: Text('List Page'),
+    return const Scaffold(
+      body: Column(
+        children: [
+          Header(),
+          Expanded(
+            child: Center(
+              child: Text('List Page'),
+            ),
+          ),
+        ],
       ),
     );
   }
