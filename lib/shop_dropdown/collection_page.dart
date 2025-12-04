462a5c9b622797fac51a319f42888f60391193ab
tonscripts <tonscripts@gmail.com>
2025-12-04 00:30:45 +0000

    creare collection

--- lib/shop_dropdown/collection_page.dart
+++ lib/shop_dropdown/collection_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/header.dart';
 
 class CollectionPage extends StatelessWidget {
   const CollectionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Collection'),
-      ),
-      body: const Center(
-        child: Text('Collection Page'),
+    return const Scaffold(
+      body: Column(
+        children: [
+          Header(),
+          Expanded(
+            child: Center(
+              child: Text('Collection Page'),
+            ),
+          ),
+        ],
       ),
     );
   }
