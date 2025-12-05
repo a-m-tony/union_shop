c91938d4e0899aa25988b3119859ee50f1207ee8
tonscripts <tonscripts@gmail.com>
2025-12-05 11:49:57 +0000

    chnage comment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -18,7 +16,7 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('1'), findsOneWidget);//
   });
 }
 
